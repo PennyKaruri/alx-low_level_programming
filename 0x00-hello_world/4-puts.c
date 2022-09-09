0671963c8841e81da75c7fa7aaa9333867ea382a
PennyKaruri <karuripenny100@gmail.com>
2022-09-08 18:03:54 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- my printing point
+ * Description: prints "Programming is like building a multilingual puzzle"
+ * Return: zero 0
+ */
+int main(void)
+{	
+	puts"\"Programming is like building a multilingual puzzle");
+	return (0);
+}
